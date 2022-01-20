9ae5970fbb96e1210a559959943bd08e6c2b23c9
tangxiaolin <tangxiaolin@loongson.cn>
2022-01-20 11:18:46 +0800

    Fix gas bug:     Segment error in compilation due to too     long symbol name.
        change "char buffer[8192];" into "char *buffer =
        (char *) malloc(1000 +  6 * len_str);"in function
        loongarch_expand_macro_with_format_map.
    
        gas/config/tc-loongarch.c
        include/opcode/loongarch.h
        opcodes/loongarch-coder.c
    
    (cherry picked from commit f0515dcf042a9b1cc4625163b0fc540ed7858661)

--- gas/config/tc-loongarch.c
+++ gas/config/tc-loongarch.c
@@ -969,7 +969,8 @@ assember_macro_helper (const char *const args[], void *context_ptr)
 	}
       while (0);
 
-      ret = loongarch_expand_macro (insns_buf, arg_strs, NULL, NULL);
+      ret = loongarch_expand_macro (insns_buf, arg_strs, NULL, NULL,
+                                    sizeof (args_buf));
     }
   return ret;
 }
@@ -981,6 +982,7 @@ static void
 loongarch_assemble_INSNs (char *str)
 {
   char *rest;
+  size_t len_str = strlen(str);
 
   for (rest = str; *rest != ';' && *rest != '\0'; rest++);
   if (*rest == ';')
@@ -1026,7 +1028,7 @@ loongarch_assemble_INSNs (char *str)
 	  char *c_str = loongarch_expand_macro (the_one.insn->macro,
 						the_one.arg_strs,
 						assember_macro_helper,
-						&the_one);
+						&the_one, len_str);
 	  loongarch_assemble_INSNs (c_str);
 	  free (c_str);
 	}
